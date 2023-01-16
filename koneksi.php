8d197103f842b885dc9642b46a1c1339fd1d5bce
san2304 <khoirulikhsan66@gmail.com>
2023-01-16 22:19:08 +0700

    lah

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+// konfigurasi database
+$host       =   "localhost";
+$user       =   "root";
+$password   =   "2304";
+$database   =   "jualan";
+// perintah php untuk akses ke database
+$koneksi = mysqli_connect($host, $user, $password, $database);
+?>
